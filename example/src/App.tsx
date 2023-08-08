a8fe48489581086e16c67623cbbe2dc943efbf62
yudimarta <yudi.marta.arianto@gmail.com>
2023-08-08 14:49:16 +0700

    fix: fix foo to enable bar
    This fixes the broken behavior of the component by doing xyz.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { AwesomeLibraryView } from 'react-native-awesome-library';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AwesomeLibraryView color="#32a852" style={styles.box} />
+      <Text>Halo Mas Fakhry Fauzan</Text>
     </View>
   );
 }
